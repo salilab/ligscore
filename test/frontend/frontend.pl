ad47990163acc0c39feb356a059acf46812df986
Ben Webb <ben@salilab.org>
2012-10-15 21:36:39 +0000

    Match latest set of links.

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -14,9 +14,9 @@ my $t = new saliweb::Test('ligscore');
     my $self = $t->make_frontend();
     my $links = $self->get_navigation_links();
     isa_ok($links, 'ARRAY', 'navigation links');
-    like($links->[0], qr#<a href="http://modbase/top/">Ligand Score Home</a>#,
-         'Index link');
+    like($links->[0],
+         qr#<a href="http://modbase/top/help.cgi\?type=about">About</a>#,
+         'About link');
     like($links->[1],
-         '/<a href="http:\/\/modbase\/top\/queue\.cgi">Ligand Score Current ' .
-         'queue<\/a>/', 'Queue link');
+         '/<a href="http:\/\/modbase\/top\/">Web Server<\/a>/', 'Index link');
 }
