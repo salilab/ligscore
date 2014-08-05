7940256db1c3c2f14b721a6176fa3e79bd96a915
Ben Webb <ben@salilab.org>
2014-08-05 17:01:50 +0000

    Test index page and header/footer.

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -20,3 +20,44 @@ my $t = new saliweb::Test('ligscore');
     like($links->[1],
          '/<a href="http:\/\/modbase\/top\/">Web Server<\/a>/', 'Index link');
 }
+
+# Test get_lab_navigation_links
+{
+    my $self = $t->make_frontend();
+    my $links = $self->get_lab_navigation_links();
+    isa_ok($links, 'ARRAY', 'lab navigation links');
+    like($links->[0],
+         qr#<a href="http.*">Sali Lab Home</a>#, 'Lab link');
+    like($links->[9],
+         '/<a href="http:\/\/shoichetlab.*">Shoichet Lab<\/a>/',
+         'Shoichet link');
+}
+
+# Test get_project_menu
+{
+    my $self = $t->make_frontend();
+    my $menu = $self->get_project_menu();
+    is($menu, "", "Project menu");
+}
+
+# Test get_header_page_title
+{
+    my $self = $t->make_frontend();
+    my $header = $self->get_header_page_title();
+    like($header, qr#a web server for scoring#, "header page title");
+}
+
+# Test get_footer
+{
+    my $self = $t->make_frontend();
+    my $footer = $self->get_footer();
+    like($footer, qr#Statistical Potential for Modeling#, "footer");
+}
+
+# Test get_index_page
+{
+    my $self = $t->make_frontend();
+    my $txt = $self->get_index_page();
+    like($txt, '/Email address.*Score type/ms',
+         "get_index_page");
+}
