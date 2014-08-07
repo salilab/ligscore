4594576aa19d1ae35654e7a53bd0acccd0b20fd3
Ben Webb <ben@salilab.org>
2014-08-07 23:43:46 +0000

    Test get_submit_parameter_help().

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -61,3 +61,11 @@ my $t = new saliweb::Test('ligscore');
     like($txt, '/Email address.*Score type/ms',
          "get_index_page");
 }
+
+# Test get_index_page
+{
+    my $self = $t->make_frontend();
+    my $help = $self->get_submit_parameter_help();
+    isa_ok($help, 'ARRAY', 'get_submit_parameter_help links');
+    is(scalar(@$help), 4, 'get_submit_parameter_help length');
+}
