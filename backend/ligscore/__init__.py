a3e1a2e3815599c350dba71cdeb719a07136c0de
Ben Webb <ben@salilab.org>
2021-02-12 16:41:27 -0800

    Drop request for 64-bit hardware
    We no longer have any 32-bit hardware, so no
    need to request 64-bit.

--- backend/ligscore/__init__.py
+++ backend/ligscore/__init__.py
@@ -17,7 +17,7 @@ lib=`python3 -c "import IMP.atom; print(IMP.atom.get_data_path('%s'))"`
 ligand_score %s %s "$lib" > score.list 2> score.log
 """ % (lib, mol2, pdb)
         r = self.runnercls(script)
-        r.set_sge_options('-l arch=lx-amd64 -l h_rt=00:10:00')
+        r.set_sge_options('-l h_rt=00:10:00')
         return r
 
 
